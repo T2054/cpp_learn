4e16e4025a7e79ee99abbd180aae94e8ef2665bb
T2054 <2054605670@qq.com>
2024-01-25 23:28:53 +0800

    update

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+#define MAXSIZE 20 
+#define OK 1
+#define ERROR 0 
+typedef int ElemType;
+typedef int Status;
+
+typedef struct
+{
+    ElemType data[MAXSIZE]; /* 数组，存储数据元素 */
+    int length;             /* 线性表当前长度 */
+}SqList;
+
+Status GetElem(SqList L,int i,ElemType *e)
+{
+    if(L.length==0 || i<1 || i>L.length)
+            return ERROR;
+    *e=L.data[i-1];
+
+    return OK;
+}
+
+Status ListInsret(SqList * L,int i,ElemType e)
+{
+    int k;
+    if(L->length == MAXSIZE)
+    return ERROR;
+
+    if(i < 1 || i > L -> length + 1)
+    return ERROR;
+    
+    if(i <= L -> length)
+    {
+        for(k = L -> length - 1;k >= i - 1;k--)
+        L->data[k+1]=L->data[k];
+    }
+    L->data[i - 1] = e;
+    L->length++;
+    return OK;
+}
+
+Status ListDelete(SqList *L,int i,ElemType *e) 
+{ 
+    int k;
+    if (L->length==0)               
+		return ERROR;
+    if (i<1 || i>L->length)         
+        return ERROR;
+    *e=L->data[i-1];
+    if (i<L->length)                
+    {
+        for(k=i;k<L->length;k++)
+			L->data[k-1]=L->data[k];
+    }
+    L->length--;
+    return OK;
+}
+
+
+
+
+
+
+
