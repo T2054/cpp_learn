07e7231426f8bfa279eae8e70465f50c7846c60b
T2054 <2054605670@qq.com>
2024-02-06 22:41:09 +0800

    date

--- fac.c
+++ fac.c
@@ -1,21 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
-unsigned fac(unsigned n){
+unsigned fac(unsigned n)
+{
     unsigned f;
-    if(n == 0)
-    f = 1;
+    if (n == 0)
+        f = 1;
     else
-    f = n * fac(n-1);
+        f = n * fac(n - 1);
     return f;
 }
 
-int f(int n){
-    if (n == 1) return 1;
-    if (n == 2) return 1;
-    else return(f(n - 1) + f(n - 2));
-
-
-}
-int main333(){
-
+int f(int n)
+{
+    if (n == 1)
+        return 1;
+    if (n == 2)
+        return 1;
+    else
+        return (f(n - 1) + f(n - 2));
 }
